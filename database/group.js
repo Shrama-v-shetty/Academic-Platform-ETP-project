8d0baf7271551c978d4f4f24bf26d83c21fc0a15
nikhithagowda945-pixel <nikhithagowda945@gmail.com>
2025-10-09 22:10:43 +0530

    Update group.js

--- database/group.js
+++ database/group.js
@@ -1,1 +1,10 @@
+const mongoose = require("mongoose");
+
+const GroupSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  mentor: { type: mongoose.Schema.Types.ObjectId, ref: "Mentor" },
+  students: [{ type: mongoose.Schema.Types.ObjectId, ref: "Student" }],
+}, { timestamps: true });
+
+module.exports = mongoose.model("Group", GroupSchema);
 
