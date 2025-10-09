ee054669034869b67b3eec0b137f140a09837fb6
nikhithagowda945-pixel <nikhithagowda945@gmail.com>
2025-10-09 21:56:01 +0530

    Update student.js

--- database/student.js
+++ database/student.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  group: { type: mongoose.Schema.Types.ObjectId, ref: "Group" },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Student", StudentSchema);
 
