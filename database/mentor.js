b321096890e932e7a658a0c4bff18bc9ac384fcd
nikhithagowda945-pixel <nikhithagowda945@gmail.com>
2025-10-09 22:06:30 +0530

    Update mentor.js

--- database/mentor.js
+++ database/mentor.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
+
+const MentorSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  expertise: String,
+  groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
+}, { timestamps: true });
+
+module.exports = mongoose.model("Mentor", MentorSchema);
 
