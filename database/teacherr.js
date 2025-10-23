c76a6812a2f7ea7e866e8fbf3d49053bb73b78ea
nikhithagowda945-pixel <nikhithagowda945@gmail.com>
2025-10-23 22:19:25 +0530

    Create teacherr.js

---
+++ database/teacherr.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const TeacherSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  department: { type: String },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Teacher", TeacherSchema);
