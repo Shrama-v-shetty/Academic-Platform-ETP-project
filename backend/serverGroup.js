a96fbaec6b7878595ce432de0cebb6ebac35e662
Shrama-v-shetty <164378865+Shrama-v-shetty@users.noreply.github.com>
2025-10-23 22:43:16 +0530

    Delete backend/serverGroup.js

--- backend/serverGroup.js
+++
@@ -1,9 +0,0 @@
-const authRoutes = require("./routes/authRoutes");
-const quizRoutes = require("./routes/quizRoutes");
-const counselingRoutes = require("./routes/counselingRoutes");
-const groupRoutes = require("./routes/groupRoutes");
-
-app.use("/api/auth", authRoutes);
-app.use("/api/quiz", quizRoutes);
-app.use("/api/counseling", counselingRoutes);
-app.use("/api/groups", groupRoutes);
